6e0e6cfcd094a1e0ff134f66c2824e2842da395c
amine-nejjar <nejjar.ahmedamine@gmail.com>
2020-12-30 16:27:57 +0100

    choices screen

--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {View, Text, StyleSheet, TouchableOpacity,Image} from 'react-native'
-import CardChoice from '../../shared/cardChoice'
 class HomeScreen extends React.Component{
     constructor(){
         super();
@@ -10,10 +9,38 @@ class HomeScreen extends React.Component{
     render(){
         return (
             <View style={styles.container}>
-            <Text>Home Screen</Text>
-                <View style={styles.logoContainer}>
-                    <Image style={styles.logoStyle} resizeMode='contain' source={require('../../../assets/logo.png')} />
-                </View>            
+                <View style={styles.top}>
+                </View>
+                <View style={styles.middle}>
+                    <View style={styles.row}>
+                        <TouchableOpacity style={styles.choiceCard}>
+                            <Image style={styles.imageStyle} resizeMode='contain' source={require('../../../assets/tomato.png')}/>
+                            <Text style={styles.fruiteName}>Tomates</Text>
+                            <Text style={styles.featureText}>(Analyses pour virus mosaique, bacterie ...)</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.choiceCard}>
+                            <Image style={styles.imageStyle} resizeMode='contain' source={require('../../../assets/potato.png')}/>
+                            <Text style={styles.fruiteName}>Pommes de terre</Text>
+                            <Text style={styles.featureText}>(Analyses les bacteries)</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.row}>
+                        <TouchableOpacity style={styles.choiceCard}>
+                            <Image style={styles.imageStyle} resizeMode='contain' source={require('../../../assets/bell-pepper.png')}/>
+                            <Text style={styles.fruiteName}>Poivrons</Text>
+                            <Text style={styles.featureText}>(Analyses pour virus mosaique, bacterie ...)</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.choiceCard}>
+                            <Image style={styles.imageStyle} resizeMode='contain' source={require('../../../assets/blueberry.png')}/>
+                            <Text style={styles.fruiteName}>Myrtille</Text>
+                            <Text style={styles.featureText}>(Analyses pour virus mosaique, bacterie ...)</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <View style={styles.bottom}>
+
+                </View>
+                
             </View>
             
           );
@@ -21,17 +48,44 @@ class HomeScreen extends React.Component{
     }
 const styles = StyleSheet.create({
     container: {
-    flex: 1,
-    alignItems: 'center',
-    backgroundColor:'#fff'
+        flex: 1,
+        alignItems: 'center',
+        justifyContent:'center'
     },
-    logoStyle:{
-        width:'85%',
+    top:{
+        flex:1,
     },
-    logoContainer:{
+    middle:{
+        flex:3,
+    },
+    bottom:{
+        flex:2
+    },
+    row:{
+        flex:1,
+        width:"97%",
+        flexDirection:'row',
+    },
+    choiceCard:{
         justifyContent:'center',
-        alignItems:'center'
+        alignItems:'center',
+        flex:1,
+        margin:5,
+        backgroundColor:'#fff',
+        elevation:5
     },
+    imageStyle:{
+        width:"90%",
+        height:"35%"
+    },
+    fruiteName:{
+        color:'#0a4f00',
+    },
+    featureText:{
+        fontSize:10,
+        textAlign:'center',
+        color:'#545556'
+    }
 
 });
 export default HomeScreen
